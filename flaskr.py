f9851369752356a4c9c18c45240113798cd9bb98
xiaoyuge161108 <862558909@qq.com>
2017-07-22 07:53:52 +0800

    first app is running

--- flaskr.py
+++ flaskr.py
@@ -1,14 +1,23 @@
 #all the imports
-from falsk import Flask
+import sqlite3
+from flask import Flask
 
 
 
 
 
 #configfile
-
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
 
 
 
 app=Flask(__name__)
+app.config.from_object(__name__)
 
+def connect_db():
+  return sqlite3.connect(app.config['DATABASE'])
+
+if __name__=='__main__':
+  app.run()

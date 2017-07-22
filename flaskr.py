11e82542f1deaeae18c6fec71d11f0502035ba1e
xiaoyuge161108 <862558909@qq.com>
2017-07-22 20:04:28 +0800

    show entries

--- flaskr.py
+++ flaskr.py
@@ -31,7 +31,7 @@ def close_db(error):
 def init_db():
   with app.app_context():
     db = get_db()
-    with app.open_resource('./schema.sql',mode='r') as f:
+    with app.open_resource('schema.sql',mode='r') as f:
       db.cursor().executescript(f.read())
     db.commit()
 
